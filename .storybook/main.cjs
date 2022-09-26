9e7a5a70add003b013673d15fe38613fd4e773d2
Yunfeng Zheng <1837115857@qq.com>
2022-09-26 14:47:18 +0800

    feat: 配置tailwind对antd的按需引入

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,3 +1,6 @@
+const path = require("path");
+const vitePluginImp = require("vite-plugin-imp");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
@@ -7,5 +10,42 @@ module.exports = {
   },
   features: {
     storyStoreV7: true,
+  },
+  async viteFinal(config, { configType: env }) {
+    // customize the Vite config here
+    const customConfig = {
+      css: {
+        preprocessorOptions: {
+          less: {
+            javascriptEnabled: true,
+          },
+          modifyVars: {
+            // 在这里自定义主题色等样式
+          },
+        },
+      },
+      resolve: {
+        alias: {
+          "@": path.resolve("src"),
+          "@assets": path.resolve("src/assets"),
+        },
+      },
+      esbuild: {
+        jsxInject: `import React from 'react'`,
+      },
+    };
+
+    config.plugins.push(
+      vitePluginImp({
+        libList: [
+          {
+            libName: "antd",
+            style: (name) => `antd/es/${name}/style`,
+          },
+        ],
+      })
+    );
+    // return the customized config
+    return { ...config, ...customConfig };
   },
 };
--- src/components/Spin/ASpin.stories.tsx
+++ src/components/Spin/ASpin.stories.tsx
@@ -1,6 +1,5 @@
 // eslint-disable-next-line import/named
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import React from "react";
 import ASpin from ".";
 
 export default {
