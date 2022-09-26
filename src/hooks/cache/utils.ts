b2b96eff0d17dbceb12f08984157ca97951902b9
YunfengZheng <47732460+yunfeng-xyl@users.noreply.github.com>
2022-09-26 22:17:31 +0800

    feat: 完善cache (#10)

---
+++ src/hooks/cache/utils.ts
@@ -0,0 +1,27 @@
+import { CacheKey, CacheResult } from "@/hooks/cache/types";
+import { ResponseError } from "@/utils";
+
+export const infinity = 5 * 60 * 1000;
+
+export const getLoadingState = <T>(defaultValue: T): CacheResult<T> => {
+  return { state: "loading", loading: true, data: defaultValue };
+};
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export const noop = () => {};
+
+export const getCacheKeys = (key?: CacheKey): CacheKey[] => {
+  if (typeof key === "undefined" || key === null) {
+    return [];
+  }
+
+  if (typeof key === "number") {
+    return [key];
+  }
+
+  return key.trim().split(/\n\s*/g);
+};
+
+export const getErrorState = <T>(defaultValue: T, error: ResponseError): CacheResult<T> => {
+  return { state: "error", loading: false, data: defaultValue, error };
+};
--- src/utils/cache.ts
+++
@@ -1,8 +0,0 @@
-import { CacheResult } from "@/hooks/cache/types";
-
-export const getLoadingState = <T>(defaultValue: T): CacheResult<T> => {
-  return { state: "loading", loading: true, data: defaultValue };
-};
-
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-export const noop = () => {};
