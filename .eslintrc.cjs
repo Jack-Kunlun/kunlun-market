24fa7ddf407b4b57bce673c9a62d69ac8ab976b6
Jack <47732460+Jack-Kunlun@users.noreply.github.com>
2023-06-21 17:59:21 +0800

    refactor: eslint配置文件重新处理，拆分为custom和react两块 (#18)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom", "turbo"],
+  // This tells ESLint to load the config from the package `eslint-config-react`
+  extends: ["react", "custom", "turbo"],
   settings: {},
   overrides: [],
 };
