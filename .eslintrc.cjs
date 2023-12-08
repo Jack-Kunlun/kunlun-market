a2f959263deafacb8bc30152944e43d410d2aae2
yunfeng <1837115857@qq.com>
2023-12-08 17:06:26 +0800

    chore: optimize lint-staged configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-react`
-  extends: ["react", "custom", "turbo"],
+  extends: ["turbo", "custom"],
   settings: {},
   overrides: [],
 };
